d7c0baa1d5c57266ce9f42f289ff794b4e620ebe
lif314 <2216155077@qq.com>
2022-05-01 23:51:42 +0800

    todo list

---
+++ 3-Vue脚手架/2-todo-list/src/App.vue
@@ -0,0 +1,145 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div id="root">
+      <div class="todo-container">
+        <div class="todo-wrap">
+          <my-header :receive="receiveDataFromHeader" />
+          <my-list
+            :todos="todos"
+            :checkTodo="checkTodo"
+            :deleteListTodo="deleteTodo"
+          />
+          <my-footer
+            :todos="todos"
+            :checkAllTodo="checkAllTodo"
+            :clearAll="clearAll"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+// 组件通信：父组件给子组件传递函数获取子组件的数据
+
+<script>
+// 引入组件
+import MyFooter from "./components/MyFooter.vue";
+import MyHeader from "./components/MyHeader.vue";
+import MyList from "./components/MyList.vue";
+
+export default {
+  name: "App",
+  components: {
+    MyFooter,
+    MyHeader,
+    MyList,
+  },
+  // 将数据存给共同的父组件： APP
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "sleep", done: true },
+        { id: "002", title: "eat", done: false },
+        { id: "003", title: "music", done: true },
+        { id: "004", title: "sing", done: false },
+        { id: "005", title: "chat", done: false },
+      ],
+    };
+  },
+  methods: {
+    // 全部删除  // 删除所有已经完成的todo
+    clearAll() {
+      if (confirm("确定删除完成的todo吗?")) {
+        this.todos = this.todos.filter((todo) => {
+          return !todo.done;
+        });
+      }
+      // this.todos  = []
+    },
+    // 全选或者全不选
+    checkAllTodo(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done;
+      });
+    },
+    // 取消勾选todo
+    // 逐层传递消息
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) {
+          todo.done = !todo.done;
+        }
+      });
+    },
+    // 将该函数传给MyHeader，调用该函数获取数据
+    receiveDataFromHeader(data) {
+      // console.log("App组件收到子组件的数据", data)
+      // 将该数据加在todos中
+      this.todos.unshift(data);
+    },
+    // 删除一个todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => {
+        return todo.id !== id;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ 3-Vue脚手架/2-todo-list/src/components/MyFooter.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="todo-footer" v-show="total">
+    <label>
+      <!-- <input type="checkbox" :checked="isAll" @click="checkAll"/> -->
+      <input type="checkbox" v-model="isAll"/>
+      <!-- v-model不能修改props,但可以修改isAll 计算属性 -->
+    </label>
+    <span> <span>已完成{{doneTotal}}</span> / 全部{{total}}</span>
+    <button class="btn btn-danger" @click='deleteTodos'>清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+  props:['todos', 'checkAllTodo', 'clearAll'],
+  computed:{
+    doneTotal(){
+        // ES6
+        return this.todos.reduce((pre, current)=>{
+            // 第1次调用的返回值是第一次的return值
+            return pre + (current.done? 1 : 0)
+        }, 0)
+        
+        // let count = 0;
+        // this.todos.forEach((todo)=>{
+        //     if(todo.done){
+        //         count++;
+        //     }
+        // })
+        // return count
+    },
+    total(){
+        return this.todos.length
+    },
+    // isAll(){
+    //     return this.doneTotal === this.total && this.total > 0
+    // }
+    // 计算属性会被修改
+     isAll:{
+         get(){
+            //  是否全部选中
+            return this.doneTotal === this.total && this.total > 0
+         },
+         set(value){
+             // 全选或者全部选
+            this.checkAllTodo(value)
+         }
+        
+    }
+  },
+  methods: {
+      // 删除所有已经完成的todo
+    deleteTodos(){
+        this.clearAll()
+    },
+    // 勾选所有
+    // checkAll(e){
+    //     this.checkAllTodo(e.target.checked)
+    // }
+  },
+};
+</script>
+
+<style scoped>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
