b04efae7cc1cfed4ffaefccaf1701bafd9904a1c
lif314 <2216155077@qq.com>
2022-05-02 22:01:41 +0800

    slot

---
+++ 3-Vue脚手架/5-slot/src/components/Category.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="category">
+    <h3>商品分类列表</h3>
+    <ul>
+      <li>xxxxx</li>
+      <li>xxxxx</li>
+      <li>xxxxx</li>
+      <li>xxxxx</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+  data() {
+    return {
+      msg: 'Hello'
+    }
+  },
+}
+</script>
+
+<style scoped>
+.category{
+  background-color: aquamarine;
+  width: 200px;
+  height: 300px;
+}
+
+h3{
+  text-align: center;
+  background-color: bisque;
+}
+</style>
